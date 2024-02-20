738eef66feabf36a8add765c3f3740577699caa6
Renato <renathyventas@gmail.com>
2024-02-19 22:05:26 -0600

    respaldo-02

---
+++ Model/Alumnos/ObjAlumnos.php
@@ -0,0 +1,29 @@
+<?php
+
+class AlumnosDisp {
+      var $Id=0;
+      private var $Nombre="SIN ESPECIFICAR";
+      var $Apellido="SIN ESPECIFICAR";
+      var $Edad=0;
+      var $Correo="SIN ESPECIFICAR";
+      var $Telefono="SIN ESPECIFICAR";
+      var $direccion="SIN ESPECIFICAR";
+      var $NIE="SIN ESPECIFICAR";
+      var $UsuarioCreacion="SIN ESPECIFICAR";
+      var $UsuarioModifica="SIN ESPECIFICAR";
+
+      function __construct() {
+
+      }
+
+      function ValidarNombre($Vmonbre='SIN ESPECIFCAR')
+      {
+          
+      }
+
+      function ValidarApeliido($VApellido='SIN ESPECIFCAR')
+      {
+          
+      }
+}
+?>
